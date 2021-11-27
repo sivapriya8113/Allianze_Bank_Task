080a4356c114fc1a2034fe861577030ae1812482
sivapriya8113 <68690248+sivapriya8113@users.noreply.github.com>
2021-11-27 21:17:30 +0530

    Update settings.py

--- Bank_Task/Bank_Task/settings.py
+++ Bank_Task/Bank_Task/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y5c92o2*esg-c@6t4!e52birg)0w7_8@r8-i#q^%kj11$qx8uk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['allianzebankdjango.herokuapp.com','127.0.0.1:8000 ']
 
 
 
